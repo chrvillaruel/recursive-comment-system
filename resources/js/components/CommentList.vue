ac6e6761354e5280a0735786fbeaf459fe36cf5d
chrvillarueldev <christianvillaruel@gmail.com>
2021-04-27 15:15:19 +0800

    Added comment list component

---
+++ resources/js/components/CommentList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <display-comment
+      v-for="comment in comments"
+      :comment="comment"
+      :collection="collection"
+      :key="comment.id"
+    >
+    </display-comment>
+  </div>
+</template>
+
+<script>
+import DiplayComment from '../components/DisplayComment.vue'
+export default {
+  props: ['comments', 'collection', 'level'],
+  name: 'CommentList',
+  components: { 'display-comment': DiplayComment },
+}
+</script>
